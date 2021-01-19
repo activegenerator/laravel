8b2f9bf597204e49ca04436b7b5ec806b20d00ce
Lennard <lennard@spring.nl>
2021-01-19 15:41:44 +0100

    Cleanup config

--- config/config.php
+++ config/config.php
@@ -17,34 +17,10 @@ return [
    * Provide the generators that should be used.
    */
   "generators" => [
-    // ActiveGenerator\Nova\Generators\NovaGenerator::class,
-    // ActiveGenerator\Nova\Generators\NovaPermissionsGenerator::class,
-    // ActiveGenerator\Laravel\Generators\ApiGenerator::class,
     ActiveGenerator\Laravel\Generators\ModelGenerator::class,
-    // ActiveGenerator\Laravel\Generators\MigrationGenerator::class,
-    // ActiveGenerator\Laravel\Generators\FactoryGenerator::class,
+    ActiveGenerator\Laravel\Generators\MigrationGenerator::class,
+    ActiveGenerator\Laravel\Generators\FactoryGenerator::class,
     // ActiveGenerator\Laravel\Generators\PolicyGenerator::class,
-    // ActiveGenerator\Laravel\Generators\SeederGenerator::class,
+    ActiveGenerator\Laravel\Generators\SeederGenerator::class,
   ],
-  /**
-   * These fields classes will provide extra functionality on the segments.
-   * They "wrap around" the segment when isMatchWithSegment() returns true
-   * There can only be one match per segment
-   */
-  "fields" => [
-    ActiveGenerator\Laravel\Models\Fields\ForeignIdField::class,
-    ActiveGenerator\Laravel\Models\Fields\IdentityField::class,
-    ActiveGenerator\Laravel\Models\Fields\TimestampsField::class,
-    ActiveGenerator\Laravel\Models\Fields\BaseField::class
-  ],
-  /**
-   * These relations classes will provide extra functionality on the segments.
-   * They "wrap around" the segment when isMatchWithSegment() returns true
-   * There can only be one match per segment
-   */
-  "relations" => [
-    ActiveGenerator\Laravel\Models\Relations\MorphToRelation::class,
-    ActiveGenerator\Laravel\Models\Relations\BelongsToRelation::class,
-    ActiveGenerator\Laravel\Models\Relations\BaseRelation::class,
-  ]
 ];
